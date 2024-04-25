83a31315aa4abbbf06c90143209e9a3dc2697dea
Pinguino232 <138746869+FelipeOrtiz-M4@users.noreply.github.com>
2024-04-25 17:46:21 -0500

    tercer

--- js/app.js
+++ js/app.js
@@ -31,3 +31,30 @@ function cos() {
 function tan() {
   pantalla.innerText = Math.tan(parseFloat(pantalla.innerText));
 }
+function convertidor(sistema) {
+  let valor = parseFloat(pantalla.innerText);
+  switch (sistema) {
+      case 'BIN':
+          pantalla.innerText = valor.toString(2);
+          break;
+      case 'OCT':
+          pantalla.innerText = valor.toString(8);
+          break;
+      case 'HEX':
+          if (!isNaN(valor)) {
+              pantalla.innerText = decimalToHex(valor);
+          } else {
+              pantalla.innerText = 'Error';
+          }
+          break;
+      case 'DEC':
+          pantalla.innerText = valor.toString(10);
+          break;
+      default:
+          break;
+  }
+}
+
+function decimalToHex(decimal) {
+  return decimal.toString(16).toUpperCase();
+}
